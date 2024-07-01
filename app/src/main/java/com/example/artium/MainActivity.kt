0ef8b9f28ef9bc4cd35dd221cb811bceaf6fbb63
Parag Dalvi <dalviparag76@gmail.com>
2024-07-01 20:00:28 +0530

    refactor

--- app/src/main/java/com/example/artium/MainActivity.kt
+++ app/src/main/java/com/example/artium/MainActivity.kt
@@ -1,26 +1,35 @@
 package com.example.artium
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.artium.call.CallActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.artium.notifications.CallNotificationData
 import com.example.artium.notifications.NotificationHelper
 import com.example.artium.ui.theme.ArtiumTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(2000)
+            NotificationHelper(this@MainActivity).showCallNotification(CallNotificationData("Incoming call", "Answer", "Parag"))
+        }
 
         setContent {
             ArtiumTheme {
@@ -38,19 +47,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            NotificationHelper(context).showCallNotification(
-                CallNotificationData(
-                    "Incomfing call",
-                    "Parag",
-                    "Parag"
-                )
-            )
-        }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
     ) {
-        Text("Show noti")
+        Text(
+            "Home page",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxSize()
+        )
     }
 }
 
